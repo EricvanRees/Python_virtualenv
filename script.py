4a646a1dd69565e9403473dc3e34ce32bd8b8ef4
Eric van Rees <ericvanrees@gmail.com>
2025-06-17 15:46:58 +0200

    second commit

---
+++ script.py
@@ -0,0 +1,22 @@
+# create a new virtual environment in the command line
+# python - m virtualenv [name of virtual env]
+
+# run virtual env
+# [name of virtual env]\Scripts\activate.bat
+
+# list modules in virtualenv:
+# pip list
+
+# create requirements.txt
+# run "pip freeze"
+# copy output
+# paste into any text editor and save as "requirements.txt"
+
+# deactivate virtual environment with
+# deactivate
+
+# delete virtual env environment and subdirs
+# run rmdir project_env /s
+
+# install packages listed in requirements.txt
+# pip install -r requirements.txt
